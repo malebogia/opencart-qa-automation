2d05e2cccdf0455304be78151ec184796513c9a9
Kiril Naydinidis <naydinidis@gmail.com>
2026-02-06 14:51:34 +0200

    Enhance ConfigReader with logging functionality
    Added logging for configuration loading and property retrieval.

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -8,19 +8,23 @@ import java.util.Properties;
  */
 public class ConfigReader {
 
+    private static final Logger logger = LogManager.getLogger(ConfigReader.class);
     private static Properties properties;
 
     static {
         try {
             String path = "src/test/resources/config.properties";
+            logger.info("Loading configuration file from path: {}", path);
+            
             FileInputStream input = new FileInputStream(path);
-
-            properties = new Properties();
             properties.load(input);
             input.close();
+            
+            logger.info("Configuration file loaded successfully");
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("Could not load config.properties file!", e);
+            logger.fatal("Failed to load config.properties file", e);
         }
     }
 
@@ -31,6 +35,12 @@ public class ConfigReader {
      * @return property value
      */
     public static String getProperty(String key) {
-        return properties.getProperty(key);
+        String value = properties.getProperty(key);
+        
+        if (value == null) {
+            logger.warn("Requested property '{}' was not found in config.properties", key);
+        }
+
+        return value;
     }
 }
